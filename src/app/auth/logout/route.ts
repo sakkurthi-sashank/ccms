f944e9ed744cfe426a582ae7c2fe0bb8b74f9d34
sakkurthi-sashank <sakkurthisashank@gmail.com>
2024-04-22 09:34:41 +0530

    fix: update types

--- src/app/auth/logout/route.ts
+++ src/app/auth/logout/route.ts
@@ -1,8 +1,7 @@
 import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
 import { NextResponse } from 'next/server'
-
-import type { Database } from '@/lib/supabase/types'
+import type { Database } from '@/types/supabase'
 
 export async function POST(request: Request) {
   const requestUrl = new URL(request.url)
